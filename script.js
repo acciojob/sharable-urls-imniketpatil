2b878fcce217afcbaee1b488ef648121095c5759
Niket Patil <96330902+imniketpatil@users.noreply.github.com>
2025-05-25 14:35:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// your code here
+let url = document.getElementById("url");
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+let button = document.getElementById("button");
+
+button.onclick = function () {
+	let nameValue = "?name="name.textContent;
+	let yearValue = year.textContent;
+	
+	let urlValue = url.textContent;
+	url.textContent = urlValue+nameValue;
+}
